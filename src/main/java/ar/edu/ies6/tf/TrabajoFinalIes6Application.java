8c4f41bcd28c188dabd742cdf54e7424b307da9b
Javier Choque <alejandroj889@gmail.com>
2024-11-14 11:23:12 -0300

    Inicializando Trabajo Final - Javier Choque

---
+++ src/main/java/ar/edu/ies6/tf/TrabajoFinalIes6Application.java
@@ -0,0 +1,13 @@
+package ar.edu.ies6.tf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoFinalIes6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabajoFinalIes6Application.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/ies6/tf/TrabajoFinalIes6ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.ies6.tf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajoFinalIes6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
