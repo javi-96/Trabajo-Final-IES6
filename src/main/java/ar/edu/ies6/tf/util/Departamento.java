2b65ac0f8a486a8e3eda2ab6cd9f927b68dfbe7e
Leonela <Leonela@10.0.12.204>
2024-11-15 20:10:01 -0300

    inicializando paquete util- leonela choque

---
+++ src/main/java/ar/edu/ies6/tf/util/Departamento.java
@@ -0,0 +1,19 @@
+package ar.edu.ies6.tf.util;
+
+public enum Departamento {
+
+	SAN_PEDRO,
+	YAVI,
+	TILCARA,
+	EL_CARMEN,
+	SANTA_BARBARA,
+	PALPALA,
+	COCHINOCA,
+	LEDESMA,
+	HUMAHUACA,
+	SAN_ANTONIO,
+	DR_MANUEL_BELGRANO,
+	SANTA_CLARA,
+
+	
+}
