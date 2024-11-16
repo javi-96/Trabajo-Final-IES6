11b680ec8f25fc32680c8ed927756b0b7fda7d78
Leonela <Leonela@10.0.12.204>
2024-11-16 18:09:27 -0300

    agregue codigo a la clase ClienteServiceImpBD - Leonela Choque

--- src/main/java/ar/edu/ies6/tf/serviceImp/ClienteServiceImpBD.java
+++ src/main/java/ar/edu/ies6/tf/serviceImp/ClienteServiceImpBD.java
@@ -1,5 +1,49 @@
 package ar.edu.ies6.tf.serviceImp;
 
-public class ClienteServiceImpBD {
+import java.util.List;
+
+import ar.edu.ies6.tf.model.Cliente;
+import ar.edu.ies6.tf.service.IClienteService;
+
+public class ClienteServiceImpBD implements IClienteService{
+
+	@Override
+	public void guardarCliente(Cliente cliente) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void eliminarCliente(String dni) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void modificarCliente(Cliente clienteModificado) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Cliente consultarCliente(String dni) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Cliente> listarTodosClientes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Cliente> listarTodosClientesActivos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	
 
 }
