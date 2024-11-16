88ed147de91d114b82574cde81c9232205c322ec
Leonela <Leonela@10.0.12.204>
2024-11-16 18:32:05 -0300

    agregue codigo a la clase ClienteServiceImpBD - Leonela Choque

--- src/main/java/ar/edu/ies6/tf/serviceImp/ClienteServiceImpBD.java
+++ src/main/java/ar/edu/ies6/tf/serviceImp/ClienteServiceImpBD.java
@@ -1,21 +1,39 @@
 package ar.edu.ies6.tf.serviceImp;
 
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 
 import ar.edu.ies6.tf.model.Cliente;
+import ar.edu.ies6.tf.repository.ClienteRepository;
 import ar.edu.ies6.tf.service.IClienteService;
 
+@Service
+@Qualifier("servicioClienteBD")
 public class ClienteServiceImpBD implements IClienteService{
 
+	@Autowired
+	ClienteRepository clienteRepository;
+	
 	@Override
 	public void guardarCliente(Cliente cliente) {
 		// TODO Auto-generated method stub
+		//nos dice q todos los docentes igresados se guardaran como activos por defecto
+		cliente.setEstado(true);
+		
+		clienteRepository.save(cliente);
 		
 	}
 
 	@Override
 	public void eliminarCliente(String dni) {
 		// TODO Auto-generated method stub
+		Optional<Cliente> clienteEncontrado = clienteRepository.findById(dni);
+		clienteEncontrado.get().setEstado(false);
+		clienteRepository.save(clienteEncontrado.get());
 		
 	}
 
@@ -28,22 +46,19 @@ public class ClienteServiceImpBD implements IClienteService{
 	@Override
 	public Cliente consultarCliente(String dni) {
 		// TODO Auto-generated method stub
-		return null;
+		return clienteRepository.findById(dni).get();
 	}
 
 	@Override
 	public List<Cliente> listarTodosClientes() {
 		// TODO Auto-generated method stub
-		return null;
+		return (List <Cliente>) clienteRepository.findAll();
 	}
 
 	@Override
 	public List<Cliente> listarTodosClientesActivos() {
 		// TODO Auto-generated method stub
-		return null;
+		return (List <Cliente>) clienteRepository.findByEstado(true);  //muestra todo los que estan activos
 	}
 	
-	
-	
-
 }
