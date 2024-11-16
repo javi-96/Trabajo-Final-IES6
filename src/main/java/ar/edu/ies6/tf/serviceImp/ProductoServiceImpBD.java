bdd3af2c6a733171d5b151489b9faf74f89d5078
YANINA <YANINA@AIWA_NA-1431>
2024-11-15 21:51:25 -0300

    cree ProductoServiceImpBD. Elisa Yaniina Liquitay

---
+++ src/main/java/ar/edu/ies6/tf/serviceImp/ProductoServiceImpBD.java
@@ -0,0 +1,47 @@
+package ar.edu.ies6.tf.serviceImp;
+
+import java.util.List;
+
+import ar.edu.ies6.tf.model.Producto;
+import ar.edu.ies6.tf.service.IProductoService;
+
+public class ProductoServiceImpBD implements IProductoService{
+
+	@Override
+	public void guardarProducto(Producto producto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void eliminarProducto(String dni) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void modificarProducto(Producto productoModificado) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Producto consultarProducto(String dni) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Producto> listarTodosProductos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Producto> listarTodosProductosActivos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
