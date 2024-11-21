a4da96017cf575bc8f64db8d58ddd9df4aeef373
YANINA <YANINA@10.0.1.166>
2024-11-20 21:21:56 -0300

    modificacion de paquetes. ElisaLiquitay

--- src/main/java/ar/edu/ies6/tf/controller/ProductoController.java
+++ src/main/java/ar/edu/ies6/tf/controller/ProductoController.java
@@ -1,5 +1,6 @@
 package ar.edu.ies6.tf.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
@@ -10,6 +11,8 @@ import org.springframework.web.servlet.ModelAndView;
 
 import ar.edu.ies6.tf.model.Producto;
 import ar.edu.ies6.tf.service.IProductoService;
+import ar.edu.ies6.tf.util.Almacenamiento;
+import ar.edu.ies6.tf.util.Ram;
 
 
 @Controller
@@ -19,64 +22,65 @@ public class ProductoController {
 		//esto es una inyeccion de dependencia
 @Autowired
 Producto unProducto; //unProducto es un objeto de la clase producto. Producto es una clase java
+
 @Qualifier ("servicioProductoBD")
 @Autowired
 IProductoService productoService;
 
-//recibe las peticiones de producto
+//recibe las peticiones de listaproducto
 
-@GetMapping ("/producto")
+@GetMapping ("/registroProducto")
 public ModelAndView getIndexWithProducto() {
 
-ModelAndView transportador = new ModelAndView ("Producto");
-transportador.addObject("Producto", unProducto);
-transportador.addObject("band", false);
+ModelAndView transportador = new ModelAndView ("registroProducto");
+transportador.addObject("producto", unProducto);
+transportador.addObject("band", true);
 
 return transportador;
 }
+
 //luego que se cargan los datos viene por post
 @PostMapping("/guardarProducto")
 public ModelAndView guardarProducto (Producto producto) {
-	//aqui creamos el servi y mandamos a guardar perroo es un problema asi que creamos una
-		//inyeccion de dependenci a
-		//DocenteServiceImp docenteService = new DocenteServiceImp() ;
 productoService.guardarProducto (producto);
 
-ModelAndView transportador = new ModelAndView("listaProductos");
-transportador.addObject("listadoProductos", productoService.listarTodosProductosActivos()); //estamos llamando al almacen de docentes
+ModelAndView transportador = new ModelAndView("listaProducto");
+transportador.addObject("listadoProducto", productoService.listarTodosProductosActivos()); //estamos llamando al almacen de docentes
 return transportador;
 }
-//Eliminar docente
+
+
+//Eliminar producto
 @GetMapping("/eliminarProducto/{id}") 
 public ModelAndView deleteProducto (@PathVariable (name = "id") String id) {
 productoService.eliminarProducto(id);
 
 //muestra el nuevo listado
-ModelAndView modelView = new ModelAndView("listaProductos");
-modelView.addObject("listadoProductos", productoService.listarTodosProductosActivos());
+ModelAndView modelView = new ModelAndView("listaProducto");
+modelView.addObject("listadoProducto", productoService.listarTodosProductosActivos());
 return modelView;
 }
+
+
 //Modificar
 @GetMapping("/modificarProducto/{id}") 
 public ModelAndView modificarProducto (@PathVariable(name = "id") String id) {
 	//el parametro ModelAndView es la vista html
 	ModelAndView modelView = new ModelAndView("producto");
 	
-	modelView.addObject("producto", productoService.consultarProducto(id) );
+	modelView.addObject("productoModificar", productoService.consultarProducto(id) );
 	modelView.addObject("band", true);
 	
 	return modelView;
 }
-@GetMapping("/listadoproductos") 
+@GetMapping("/listadoProductos") 
 public ModelAndView getAllProducto () {
 
-ModelAndView transportador = new ModelAndView("ListadoProductos");
-transportador.addObject("listado", productoService.listarTodosProductosActivos());
+ModelAndView transportador = new ModelAndView("listaProducto");
+transportador.addObject("listadoProducto", productoService.listarTodosProductosActivos());
 
 return transportador;}
 
 
-}
-	
-	
+}	
 
